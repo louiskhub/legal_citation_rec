79f015a5a3aeb17e8b54c31ae9abacd81e8288f9
Louis Kapp <lkapp@uni-osnabrueck.de>
2023-05-20 19:36:49 +0300

    change onehot encoding

--- dataset_single_file.py
+++ dataset_single_file.py
@@ -22,18 +22,15 @@ class CitationDataset(Dataset):
     ) -> None:
         self.fp: str = os.path.join(data_dir, f"vocab_size_{vsize}", set_type)
         self.inputs: torch.IntTensor = torch.load(os.path.join(self.fp, "inputs.pt"))
-        print(torch.load(os.path.join(self.fp, "labels.pt")))
-        self.labels: torch.LongTensor = one_hot(
-            torch.load(os.path.join(self.fp, "labels.pt")).long(),
-            num_classes=vsize,
-        )
+        self.labels: torch.IntTensor = torch.load(os.path.join(self.fp, "labels.pt"))
+        self.onehot: torch.Tensor = torch.eye(vsize, dtype=torch.int32)
 
     def __len__(self) -> int:
         """Returns the size of the dataset."""
         return len(self.labels)
 
     def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
-        return (  # distilbert needs int32/int62 tensors
+        return (  # distilbert needs int32 tensors
             self.inputs[idx].int(),
-            self.labels[idx],
+            self.onehot[self.labels[idx]],
         )
